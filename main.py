5386cead7a395b788fc9937a3bee01545362fdc0
nazmul629 <nazmulhossain.qnh@gmail.com>
2021-11-22 00:02:56 +0600

    Plear fire Bullet

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import random
 from pygame.constants import KEYDOWN
 from pygame.image import load
- 
+#   asdasda
 pygame.init()
 
 screen = pygame.display.set_mode((800,600))
@@ -14,9 +14,9 @@ icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
 player_img =  pygame.image.load('player.png')
-PlayerX = 370
+playerX = 370
 playerY = 480
-PlayerX_change = 0
+playerX_change = 0
 
 enemy_img =  pygame.image.load('enemy.png')
 enemyX = random.randint(0,800)
@@ -24,12 +24,24 @@ enemyY = random.randint(50,150)
 enemyX_change = 1.5
 enemyY_change = 40
 
+bullet_img =  pygame.image.load('bullet.png')
+bulletX = 0
+bulletY = 480
+bulletX_change = 0
+bulletY_change = 5
+bullet_state = 'ready'
+
 
 def player(x,y):
     screen.blit(player_img,(x,y))
 
 def enemy(x,y):
     screen.blit(enemy_img,(x,y))
+
+def fire_bullet(x,y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bullet_img,(x+16,y+10))
 
 
 running = True
@@ -42,25 +54,28 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                PlayerX_change = -4
+                playerX_change = -4
 
             if event.key == pygame.K_RIGHT:
-                PlayerX_change = 4
+                playerX_change = 4
+
+            if event.key == pygame.K_SPACE:
+                fire_bullet(playerX,bulletY)
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                PlayerX_change = 0
+                playerX_change = 0
 
 # Pleayer Chacking The boundary od Spacesho[ sp it doesn't go out od voundarys
 
-    PlayerX += PlayerX_change
-    if PlayerX<=0:
-        PlayerX = 0
-    elif PlayerX>=756:
-        PlayerX = 756
+    playerX += playerX_change
+    if playerX<=0:
+        playerX = 0
+    elif playerX>=756:
+        playerX = 756
 
 
-    # Enams movement 
+    # Enameys movement 
 
     enemyX += enemyX_change
     if enemyX <=0:
@@ -70,7 +85,14 @@ while running:
         enemyX_change = -1.5
         enemyY +=enemyY_change 
 
+    # Bullet Movement 
+    if bullet_state is "fire":
+        fire_bullet(playerX,bulletY)
+        bulletY -= bulletY_change
+
+       
  
-    player(PlayerX,playerY)
-    enemy(enemyX,enemyY)
+ 
+    player(playerX,playerY)
+    enemy(enemyX,enemyY) 
     pygame.display.update()
